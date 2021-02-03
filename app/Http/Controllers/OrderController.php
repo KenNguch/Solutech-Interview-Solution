3177e21920c59f8785c64093689fc6056b9b7ed7
Kennedy Ngure <kngure@fintech-group.com>
2021-02-03 16:17:23 +0300

    Added Validations(Requests) and did OrderApi)

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,84 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrdersRequest;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+
+        return response()->json(Order::paginate(), Response::HTTP_OK);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(OrdersRequest $request)
     {
-        //
+        $order = Order::create([
+
+            $request->only('order_number')
+
+        ]);
+        return response($order, Response::HTTP_CREATED);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function show($id)
     {
-        //
+        return response()->json(Order::find($id));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Models\Order $order
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Order $order)
+    public function update(OrdersRequest $request, $order)
     {
-        //
+        $order = Order::find($id);
+
+        $order->update($request->only('first_name', 'lastname', 'email'));
+
+        return response($order, Response::HTTP_ACCEPTED);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\Order $order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order $order)
+      public function destroy($id)
     {
-        //
-    }
+        Order::destroy($id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Order $order
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Order $order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Order $order
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Order $order)
-    {
-        //
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }

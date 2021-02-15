5147c5cb20f70b6ff7cbc39e03ad51a54b2dc68e
Kennedy Ngure <kennguch3@gmail.com>
2021-02-15 15:18:43 +0300

    Documentation For Fetch Order By ID

--- app/Http/Controllers/Apis/OrderController.php
+++ app/Http/Controllers/Apis/OrderController.php
@@ -74,6 +74,41 @@ class OrderController extends Controller
         return response($order, Response::HTTP_CREATED);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/orders/{id}",
+     *      operationId="getOrderById",
+     *      tags={"Orders"},
+     *      summary="Get order information",
+     *      description="Returns order data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Orders id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
     public function show($id)
     {
         return response()->json(Order::find($id));

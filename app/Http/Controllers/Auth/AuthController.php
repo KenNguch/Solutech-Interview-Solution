3519273a5ecaded865fcfb138d9e005e2a9246ac
Kennedy Ngure <kennguch3@gmail.com>
2021-02-16 17:01:12 +0300

    gitignore fixed|

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -122,7 +122,7 @@ class AuthController extends Controller
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
         if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->expires_at = Carbon::now()->addHours(3);
         $token->save();
         return response()->json([
             'access_token' => $tokenResult->accessToken,
